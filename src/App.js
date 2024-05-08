231b13b4988e52e59a481c7c8e74a082b177e42f
Farooq Basheer <farooqbasheer000@gmail.com>
2024-05-08 23:34:21 +0530

    import

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 import About from './components/about/About';
-import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import useLocalStorage from 'use-local-storage'
 import TabTitleChanger from './utils/tabtitlechanger';
